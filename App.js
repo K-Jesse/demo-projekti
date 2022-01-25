a3c7ac9fb9648375d4396802dff55e1d504c80e0
Jesse <jesse@kulmala.io>
2022-01-25 06:37:52 +0200

    Ei oo enään niin sekasin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>lollo</Text>
+      <Text>Homma toimii onneks</Text>
       <StatusBar style="auto" />
     </View>
   );
